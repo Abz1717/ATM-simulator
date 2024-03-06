ae0cbbd1a2a99d0bdb2467415888e4c31b1d3a4c
Abz1717 <2457570@dundee.ac.uk>
2024-03-06 09:40:20 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ATM_simulator
+{
+    partial class ATMForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAccNum = new System.Windows.Forms.TextBox();
+            this.txtPin = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAccNum
+            // 
+            this.txtAccNum.Location = new System.Drawing.Point(417, 175);
+            this.txtAccNum.Name = "txtAccNum";
+            this.txtAccNum.Size = new System.Drawing.Size(378, 31);
+            this.txtAccNum.TabIndex = 0;
+            // 
+            // txtPin
+            // 
+            this.txtPin.Location = new System.Drawing.Point(417, 258);
+            this.txtPin.Name = "txtPin";
+            this.txtPin.Size = new System.Drawing.Size(378, 31);
+            this.txtPin.TabIndex = 1;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(417, 363);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(377, 74);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // ATMForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 749);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtPin);
+            this.Controls.Add(this.txtAccNum);
+            this.Name = "ATMForm";
+            this.Text = "ATM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAccNum;
+        private System.Windows.Forms.TextBox txtPin;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_simulator
+{
+
+
+
+    public partial class ATMForm : Form
+    {
+
+        private Account activeAccount = null;
+        private Button btnWithdraw;
+        private Button btnCheckBalance;
+        private Button btnLogout;
+        private Button btnNewAtm;
+        private Label lblBalance;
+
+
+
+        private void Initializing()
+        {
+            btnWithdraw = new Button { Text = "Withdraw", Visible = false, Location = new Point(10, 110), Size = new Size(200, 30) };
+            btnCheckBalance = new Button { Text = "Check Balance", Visible = false, Location = new Point(10, 150), Size = new Size(200, 30) };
+            btnLogout = new Button { Text = "Logout", Visible = false, Location = new Point(10, 190), Size = new Size(200, 30) };
+            btnNewAtm = new Button { Text = "New ATM", Visible = false, Location = new Point(10, 270), Size = new Size(200, 30) };
+            lblBalance = new Label { Location = new Point(10, 230), Size = new Size(200, 30), Visible = false };
+
+
+            btnWithdraw.Click += new EventHandler(this.btnWithdraw_Click);
+            btnCheckBalance.Click += new EventHandler(this.btnCheckBalance_Click);
+            btnLogout.Click += new EventHandler(this.btnLogout_Click);
+            btnNewAtm.Click += new EventHandler(this.btnNewAtm_Click);
+
+            Controls.Add(lblBalance);
+            Controls.Add(btnWithdraw);
+            Controls.Add(btnCheckBalance);
+            Controls.Add(btnLogout);
+        }
+
+        private void btnNewAtm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("New ATM Clicked - Functionality to be implemented.");
+
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Controls.Clear();
+        }
+
+        private void btnCheckBalance_Click(object sender, EventArgs e)
+        {
+            lblBalance.Visible = true;
+
+            if (this.activeAccount != null)
+            {
+                this.lblBalance.Text = " your current balance is : " + activeAccount.getBalance();
+            }
+        }
+
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public ATMForm()
+        {
+            InitializeComponent();
+            Initializing();
+
+        }
+
+
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtAccNum.Text) || string.IsNullOrWhiteSpace(txtPin.Text))
+            {
+                MessageBox.Show("Enter your account number and PIN before pressing confirm");
+                return; 
+            }
+
+            if (!int.TryParse(txtAccNum.Text, out int accountNumber) || accountNumber <= 0)
+            {
+                MessageBox.Show("Please enter a valid account number.");
+                return;
+            }
+
+            if (!int.TryParse(txtPin.Text, out int pin) || pin <= 0)
+            {
+                MessageBox.Show("Please enter a valid PIN.");
+                return; 
+            }
+
+
+            ATM atm = new ATM();
+            activeAccount = atm.findAccount(int.Parse(txtAccNum.Text));
+            if (activeAccount != null)
+            {
+                if (activeAccount.checkPin(int.Parse(txtPin.Text)))
+                {
+                    MenuAfterConfirmation();
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid PIN");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Account not found");
+            }
+        }
+
+
+
+        private void MenuAfterConfirmation()
+        {
+            btnWithdraw.Visible = true;
+            btnCheckBalance.Visible = true;
+            btnLogout.Visible = true;
+            btnNewAtm.Visible = true;
+
+            txtAccNum.Visible = false;
+            txtPin.Visible = false;
+            btnConfirm.Visible = false;
+
+            if (Visible == false)
+            {
+                txtAccNum.Clear();
+                txtPin.Clear();
+            }
+        }
+        class ATM
+        {
+            private Account[] ac = new Account[3];
+
+            public ATM()
+            {
+                ac[0] = new Account(300, 1111, 111111);
+                ac[1] = new Account(750, 2222, 222222);
+                ac[2] = new Account(3000, 3333, 333333);
+
+
+
+            }
+
+
+            public Account findAccount(int accountNumber)
+            {
+                foreach (Account acc in ac)
+                {
+                    if (acc.GetAccountNum() == accountNumber)
+                    {
+                        return acc;
+                    }
+                }
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    class Account
+    {
+        //the attributes for the account
+        private int balance;
+        private int pin;
+        private int accountNum;
+
+        // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
+        public Account(int balance, int pin, int accountNum)
+        {
+            this.balance = balance;
+            this.pin = pin;
+            this.accountNum = accountNum;
+        }
+
+        //getter and setter functions for balance
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void setBalance(int newBalance)
+        {
+            this.balance = newBalance;
+        }
+
+
+        public Boolean decrementBalance(int amount)
+        {
+            if (this.balance > amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public Boolean checkPin(int pinEntered)
+        {
+            if (pinEntered == pin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int GetAccountNum()
+        {
+            return accountNum;
+        }
+
+    }
+}
+
